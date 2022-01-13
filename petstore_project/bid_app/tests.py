e86830c79a0866a45a25964824b9aa8375031d4e
Mohamed Elhwary <mh.hwary@yahoo.com>
2022-01-14 00:16:36 +0200

    add test SetBidsTest for user_bid_on_pet endpoint

--- petstore_project/bid_app/tests.py
+++ petstore_project/bid_app/tests.py
@@ -1,9 +1,40 @@
+from datetime import date
 import json
+from django.http import response
 from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 from .models import PetBid, Pet
+
+
+class SetBidTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="user_1")
+        self.pet = Pet.objects.create(owner_id=self.user)
+        self.client = APIClient() 
+
+
+    def test_user_bid_on_pet(self):
+        data = {
+            "pet_owner" : self.user.id,
+            "pet_bidder" : self.user.id,
+            "pet_id" : self.pet.id,
+            "price" : 190.00
+        }
+        response = self.client.generic(
+            method = 'POST',
+            path = '/bids/bid_on_pet/',
+            data = json.dumps(data),
+            content_type = "application/json"
+        )
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['pet_id'], self.pet.id)
+        self.assertEqual(response.data['pet_owner'], self.user.id)
+        self.assertEqual(response.data['price'], '190.00')
+
 
 
 class GetBidsTest(TestCase):
